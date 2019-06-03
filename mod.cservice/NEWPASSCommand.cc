61600b42b5006757ad5ec5184d715e321825f225
QueenElsa <krystinalynn@outlook.com>
2019-06-02 22:35:27 -0400

    Update NEWPASSCommand.cc
    Backported ability for CService Admins to reset passwords of other users on IRC. Requires * access of 750 or greater.
    Function coded by Seven. This backport does not require any special modifications to the database schema.

--- mod.cservice/NEWPASSCommand.cc
+++ mod.cservice/NEWPASSCommand.cc
@@ -32,6 +32,9 @@
 #include	"responses.h"
 #include	"networkData.h"
 #include	"cservice_config.h"
+#include	"levels.h"
+
+const char NEWPASSCommand_cc_rcsId[] = "$Id: NEWPASSCommand.cc,v 1.17 2005/11/17 22:20:57 kewlio Exp $" ;
 
 namespace gnuworld
 {
@@ -69,98 +72,15 @@ if (!tmpUser)
 	return false;
 	}
 
-/* Try and stop people using an invalid syntax.. */
-if ( (string_lower(st[1]) == string_lower(tmpUser->getUserName()))
-	  || (string_lower(st[1]) == string_lower(theClient->getNickName())) )
+int admAccess = (int)bot->getAdminAccessLevel(tmpUser);
+string newpass = st.assemble(1);
+sqlUser* targetUser = tmpUser;
+//bool notMe = false;
+if ((admAccess) && (st.size() > 2))
+{
+	targetUser = bot->getUserRecord(st[1]);
+	if (targetUser)
 	{
-	bot->Notice(theClient,
-		bot->getResponse(tmpUser,
-			language::pass_cant_be_nick,
-			string("Your passphrase cannot be your username or current nick - syntax is: NEWPASS <new passphrase>")));
-	return false;
-	}
-
-if (st.assemble(1).size() > 50)
-	{
-	bot->Notice(theClient, "Your passphrase cannot exceed 50 characters.");
-	return false;
-	}
-
-if (st.assemble(1).size() < 6)
-	{
-	bot->Notice(theClient, "Your passphrase cannot be less than 6 characters.");
-	return false;
-	}
-
-
-/* Work out some salt. */
-string salt;
-
-// TODO: Why calling srand() here?
-srand(clock() * 1000000);
-
-// TODO: What is the significance of '8' here?
-// Schema states a fixed 8 bytes of random salt are used in generating the
-// passowrd.
-for ( unsigned short int i = 0 ; i < 8; i++)
-	{
-	int randNo = 1+(int) (64.0*rand()/(RAND_MAX+1.0));
-	salt += validChars[randNo];
-	}
-
-/* Work out a MD5 hash of our salt + password */
-
-md5	hash; // MD5 hash algorithm object.
-md5Digest digest; // MD5Digest algorithm object.
-
-// Prepend the salt to the password
-string newPass = salt + st.assemble(1);
-
-// Take the md5 hash of this newPass string
-hash.update( (const unsigned char *)newPass.c_str(), newPass.size() );
-hash.report( digest );
-
-/* Convert to Hex */
-
-int data[ MD5_DIGEST_LENGTH ] = { 0 } ;
-for( size_t i = 0 ; i < MD5_DIGEST_LENGTH ; ++i )
-	{
-	data[ i ] = digest[ i ] ;
-	}
-
-stringstream output;
-output << std::hex;
-output.fill('0');
-for( size_t ii = 0; ii < MD5_DIGEST_LENGTH; ii++ )
-	{
-	output << std::setw(2) << data[ii];
-	}
-output << ends;
-
-// Prepend the md5 hash to the salt
-string finalPassword = salt + output.str().c_str();
-tmpUser->setPassword(finalPassword);
-
-if( tmpUser->commit(theClient) )
-	{
-	bot->Notice(theClient,
-		bot->getResponse(tmpUser,
-			language::pass_changed,
-			string("Password successfully changed.")));
-
-	bot->InsertUserHistory(theClient, "NEWPASS");
-	}
-else
-	{
-	// TODO
-	bot->Notice( theClient,
-		"NEWPASS: Unable to commit to database" ) ;
-	}
-
-return true;
-
-#endif
-
-}
-
-} // namespace gnuworld.
+		if (admAccess < level::newpass)
+		{
+			
